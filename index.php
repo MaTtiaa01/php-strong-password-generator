9e6b63f35d6199713a447144c7e4f6f6282166ec
MaTtiaa01 <mattia2001@live.it>
2022-11-29 16:35:53 +0100

    add characther chose yes or no

--- index.php
+++ index.php
@@ -3,12 +3,25 @@
 include __DIR__ . '/partials/functions.php';
 
 
+
+$password = '';
 if (isset($_GET['length']) && is_numeric($_GET['length'])) {
-    $password = generatePassword($_GET['length']);
-    var_dump($password);
+    if (!isset($_GET['true']) && !isset($_GET['false'])) {
+        $password = 'choose yes or no';
+    } elseif (isset($_GET['true'])) {
+        $password = generatePassword($_GET['length'], $_GET['true']);
+    } elseif (!isset($_GET['true'])) {
+        $true = '.';
+        $password = generatePassword($_GET['length'], $true);
+    }
 } else {
     echo 'Type a number';
 }
+var_dump($password);
+
+
+
+
 
 ?>
 
@@ -19,16 +32,61 @@ if (isset($_GET['length']) && is_numeric($_GET['length'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>password generator</title>
 </head>
 
 <body>
 
-    <div class="container">
-        <form action="index.php" method="get">
-            <input type="text" name="length" id="length" placeholder="Type a number">
-            <button type="submit">Generate Password</button>
-        </form>
+    <div class="container p-4 text-center">
+        <header>
+            <h1 class="text-uppercase my-2">Password generetor</h1>
+        </header>
+        <main>
+            <form action="index.php" method="get">
+                <div>
+                    <label for="length">Lunghezza Password</label>
+                    <input type="text" name="length" id="length" placeholder="Type a number">
+                </div>
+
+                <div>Dare la possibilità di ripetere i caratteri</div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="true" id="true">
+                    <label class="form-check-label" for="true">
+                        si
+                    </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="false" id="false">
+                    <label class="form-check-label" for="false">
+                        no
+                    </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" name="symbols" id="symbols">
+                    <label class="form-check-label" for="symbols">
+                        Simboli
+                    </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" name="letter" id="letter" checked>
+                    <label class="form-check-label" for="letter">
+                        Lettere
+                    </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" name="numbers" id="numbers" checked>
+                    <label class="form-check-label" for="numbers">
+                        Numeri
+                    </label>
+                </div>
+
+                <button class="btn btn-sm btn-primary" type="submit">Generate Password</button>
+                <button class="btn btn-sm btn-danger" type="reset">Reset</button>
+
+            </form>
+            <h2><?php echo $password ?> </h2>
+        </main>
 
     </div>
 
--- partials/functions.php
+++ partials/functions.php
@@ -1,12 +1,35 @@
 <?php
 
-function generatePassword($length)
+// function generatePassword($length)
+// {
+//     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+//     $charactersLength = strlen($characters);
+//     $randomString = '';
+//     for ($i = 0; $i < $length; $i++) {
+//         $randomString .= $characters[rand(0, $charactersLength - 1)];
+//     }
+//     return $randomString;
+// };
+
+function generatePassword($length, $true)
 {
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $charactersLength = strlen($characters);
     $randomString = '';
-    for ($i = 0; $i < $length; $i++) {
-        $randomString .= $characters[rand(0, $charactersLength - 1)];
+
+    if ($true === 'on') {
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+    } else {
+        for ($i = 0; $i < $length; $i++) {
+            $randomChar = $characters[rand(0, $charactersLength - 1)];
+            if (!str_contains($randomString, $randomChar)) {
+                $randomString .= $characters[rand(0, $charactersLength - 1)];
+            }
+        }
     }
+
+
     return $randomString;
 };
